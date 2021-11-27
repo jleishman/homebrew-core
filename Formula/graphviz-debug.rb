db73ae48064179f8710afaa3f0c9da4b0d196986
Justin Leishman <justin@firmaneue.com>
2021-11-27 18:56:41 -0500

    Tweaks Formula for GraphvizDebug

--- Formula/graphviz-debug.rb
+++ Formula/graphviz-debug.rb
@@ -1,4 +1,4 @@
-class Graphviz < Formula
+class GraphvizDebug < Formula
   desc "Graph visualization software from AT&T and Bell Labs"
   homepage "https://www.graphviz.org/"
   url "https://gitlab.com/graphviz/graphviz.git",
@@ -7,15 +7,6 @@ class Graphviz < Formula
   license "EPL-1.0"
   version_scheme 1
   head "https://gitlab.com/graphviz/graphviz.git"
-
-  bottle do
-    rebuild 1
-    sha256 arm64_big_sur: "6b66e92e9f9e1abee4ea21839bb137f9754526994c41dcd952f5b15bfcd4c386"
-    sha256 monterey:      "1a2fb506d039b671ac4b7502db53a647d215b7c0515215703e70648e485a7528"
-    sha256 big_sur:       "cfb8c0621bd74e2517b66ab3df6845feec5bf9468ec18274924ce3bccb15f3ce"
-    sha256 catalina:      "bd8cd2241e0e4dd5c763093852628c16c7d0fac5929592e1f46c208587e71bb6"
-    sha256 x86_64_linux:  "e311e8fba55dd28e22e12ed2635c9aa1f5ee4c725884337d15b2ca9da47e38f1"
-  end
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
